6f8d2be276d71fef0e2e17b14277558b4f4a5ccc
anthony <tony20402014@gmail.com>
2024-03-09 15:03:19 -0600

    token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,81 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     function verifyCode(Request $request) {
         
     }
+
+    public function register(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8'
+        ],
+        [
+            'name.required' => 'El nombre es requerido',
+            'name.min' => 'El nombre debe tener al menos 3 caracteres',
+            'name.max' => 'El nombre debe tener máximo 255 caracteres',
+            'email.required' => 'El email es requerido',
+            'email.email' => 'El email debe ser un email válido',
+            'email.unique' => 'El email ya está en uso',
+            'password.required' => 'El password es requerido',
+            'password.min' => 'El password debe tener al menos 8 caracteres'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ], 400);
+        }
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return response()->json([
+            'message' => 'User created successfully'
+        ], 201);
+    }
+
+    public function login(Request $request){
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user){
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404);
+        }
+
+        if(! $user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'msg' => 'Contraseña incorrecta'
+            ], 401);
+        }
+
+        $token = $user->createToken('Accesstoken')->plainTextToken;
+
+        return response()->json([
+            'msg' => 'Se ha logeado correctamente',
+            'data' => $user,
+            'jwt' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['status' => true]);
+    }
+ 
+
 }
