c2da15b8b8c198949b5bd0803309b3321b87a34e
Marco <marco1102004@gmail.com>
2024-03-09 16:59:37 -0600

    merge data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,27 +18,30 @@ class UserController extends Controller
     }
 
     function verifyCode(Request $request) {
-        $usuario_id = $request->input('userId');
-        $codigo_ingresado = $request->input('codigo');
+        $token = $request->token;
+        return response()->json(['mensaje' => 'Código', 'token' => $token]);
         
-        if(!$usuario_id || !$codigo_ingresado){
-            return response()->json(['mensaje' => 'Datos inválidos'], 400);
-        }
-
-        if(!User::find($usuario_id)){
-            return response()->json(['mensaje' => 'Usuario no encontrado'], 404);
-        }
-
-        if(!Cache::has('codigo_' . $usuario_id)){
-            return response()->json(['mensaje' => 'Código expirado'], 400);
-        }
+        // $usuario_id = $request->input('userId');
+        // $codigo_ingresado = $request->input('codigo');
         
-        $codigo_guardado = Cache::get('codigo_' . $usuario_id);
+        // if(!$usuario_id || !$codigo_ingresado){
+        //     return response()->json(['mensaje' => 'Datos inválidos'], 400);
+        // }
 
-        if ($codigo_guardado && $codigo_guardado == $codigo_ingresado) {
-            Cache::forget('codigo_' . $usuario_id);
-            return response()->json(['mensaje' => 'Código válido']);
-        }
-        return response()->json(['mensaje' => 'Código inválido'], 400);
+        // if(!User::find($usuario_id)){
+        //     return response()->json(['mensaje' => 'Usuario no encontrado'], 404);
+        // }
+
+        // if(!Cache::has('codigo_' . $usuario_id)){
+        //     return response()->json(['mensaje' => 'Codigo no valido', "userId" => $codigo_ingresado], 400);
+        // }
+
+        // $codigo_guardado = Cache::get('codigo_' . $usuario_id);
+
+        // if ($codigo_guardado && $codigo_guardado == $codigo_ingresado) {
+        //     Cache::forget('codigo_' . $usuario_id);
+        //     return response()->json(['mensaje' => 'Código válido']);
+        // }
+        // return response()->json(['mensaje' => 'Código inválido'], 400);
     }
 }
