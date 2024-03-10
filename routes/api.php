4e644db613c4052ce1a73158c67c8be3a4056e92
Alonso <abrahamreynoso64@gmail.com>
2024-03-10 16:21:31 -0600

    Endpoint

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
 
 Route::post('/pet/store', [PetController::class, 'store']);
+Route::get('/pet/index/{id}', [PetController::class, 'getPetsByUser'])->where('id', '[0-9]+');
 Route::post('/specie/store', [SpecieController::class, 'store']);
 Route::get('/specie/index', [SpecieController::class, 'index']);
 Route::post('/vetappointment/store', [VetAppointmentController::class, 'store']);
