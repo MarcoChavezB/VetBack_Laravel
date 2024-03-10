00e20abbe593ba914d83e24d3ed9202cf99aba9c
Marco <marco1102004@gmail.com>
2024-03-09 19:35:56 -0600

    redirigiendo a code

--- resources/views/emails/VerificationCodeView.blade.php
+++ resources/views/emails/VerificationCodeView.blade.php
@@ -1,24 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Confirmation Letter</title>
+    <title>One-time verification code</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f9f9f9;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+            text-align: center;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 15px;
+            box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+            margin-top: 50px;
+        }
+        h2 {
+            color: #446dff;
+        }
+        p {
+            color: #333;
+        }
+        a {
+            display: inline-block;
+            color: #000000;
+            background-color: #446dff;
+            padding: 10px 20px;
+            margin: 20px 0;
+            border-radius: 5px;
+            text-decoration: none;
+        }
+    </style>
 </head>
-
-<body style="font-family: 'Arial', sans-serif; background-color: #f0f0f0; padding: 20px;">
-
-    <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" style="width: 100%; max-width: 600px; background-color: #ffffff; border-radius: 8px; overflow: hidden; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-        <tr>
-            <td style="padding: 20px; text-align: center;">
-                <h1>⭐ Star Cabo Transportations</h1>
-                <h2 style="color: #27ae60; margin-bottom: 20px;">Request Confirmation</h2>
-                <p>Código: {{ $code }}</p>
-              </td>
-        </tr>
-    </table>
-
+<body>
+<div class="container">
+    <h2>Verificacion</h2>
+    <p>Por favor ingrese este codigo en la aplicacion.</p>
+    <p>Este codigo es valido por 5 min</p>
+    <a>{{$code}}</a>
+</div>
 </body>
-
 </html>
