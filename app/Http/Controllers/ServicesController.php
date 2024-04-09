3c781b75e8025f33b6fb1730a328ad7729af550e
Alonso <abrahamreynoso64@gmail.com>
2024-04-09 14:41:20 -0600

    Añadiendo header de CORS

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -82,6 +82,7 @@ class ServicesController extends Controller
         header('Content-Type: text/event-stream');
         header('Cache-Control: no-cache');
         header('Connection: keep-alive');
+        header('Access-Control-Allow-Origin: *');
 
         try {
             echo 'data:' . json_encode($message) . "\n\n";
