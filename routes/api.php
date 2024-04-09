b0981564898c0a6c4dff90ebb1807f5e75761cc3
anthony <tony20402014@gmail.com>
2024-04-09 14:41:51 -0600

    modify endpoint

--- routes/api.php
+++ routes/api.php
@@ -265,7 +265,6 @@ Route::middleware(['auth:sanctum'])->group(function () { // verifica el token
 
                             Route::middleware(['admin.auth'])->group(function () {
                                 Route::delete('/destroy/{id}', [ServicesController::class, 'destroy'])->where('id', '[0-9]+');
-                                Route::post('/sse', [ServicesController::class, 'sse']);
                         });
                     });
 
@@ -276,4 +275,6 @@ Route::middleware(['auth:sanctum'])->group(function () { // verifica el token
 
     });
 });
+
+Route::get('/services/sse', [ServicesController::class, 'sse']);
 
